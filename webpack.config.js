d44e999f956b6d249496a30cc115b5c04389ea08
PICKLE <98971557+pickle69420@users.noreply.github.com>
2023-05-08 19:27:47 +0000

    moveover for easier development

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   mode: 'development',
   devServer: {
-    static: '../',
+    static: './',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
